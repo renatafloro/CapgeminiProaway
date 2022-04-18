2c97082462279e737da655fa5a2c8438c7d7ba27
Floro <renata.floro@capgemini.com>
2022-04-18 08:26:51 -0300

    Aula 13-04

---
+++ Aula13-04/scripts/classes.js
@@ -0,0 +1,50 @@
+var urlBase = "http://localhost:3000/contatos"
+
+class Contato {
+    constructor(id, nome, fone, email) {
+        this.id = id
+        this.nome = nome
+        this.fone = fone
+        this.email = email
+    }
+    salvar() {
+        fetch(urlBase, {
+                method: 'POST',
+                body: JSON.stringify(this),
+                headers: {
+                    'Content-type': "Application/json"
+                }
+            })
+            .catch(erro => console.log(erro))
+    }
+
+    //Consulta todos
+    consultarTodos(display) {
+        fetch(urlBase)
+            .then(x => x.text())
+            .then(data => display(data))
+    }
+
+    consultarPeloId(idcontato, display) {
+        fetch(`${urlBase}/${idcontato}`)
+            .then(x => x.text())
+            .then(data => display(data))
+    }
+
+    update(){
+        fetch(`${urlBase}/${this.id}`,{
+            method: 'PUT',
+            body: JSON.stringify(this),
+            headers:{
+                'Content-type': 'Application/json'
+            }
+        })
+    }
+
+    excluir(idcontato){
+        fetch(`${urlBase}/${idcontato}`,{
+            method: 'DELETE',
+        })
+    }
+
+}
