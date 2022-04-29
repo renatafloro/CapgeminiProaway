79b729e970aa5ea9221b8dacc15ab2c234d41e93
Floro <renata.floro@capgemini.com>
2022-04-29 16:29:08 -0300

    Aula 29-04

---
+++ Aula29-04/app-testes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { ConsultaProdutoComponent } from './consulta-produto/consulta-produto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroProdutoComponent,
+    ConsultaProdutoComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Aula29-04/app-testes/src/app/cadastro-produto/cadastro-produto.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduto } from 'Interface/interface';
+
+@Component({
+  selector: 'app-cadastro-produto',
+  templateUrl: './cadastro-produto.component.html',
+  styleUrls: ['./cadastro-produto.component.css']
+})
+export class CadastroProdutoComponent implements OnInit {
+  
+  frase: string = "ola muNDo daNAdo";
+
+  produto: IProduto = {
+     id:0,
+     descricao: "Feijão preto",
+     preco: 7.85,
+     estoque: 9.8
+  }
+
+  hoje = new Date();
+
+  descricao: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
